0599f2591b729cf98f9e6076d7834e72a087bb7a
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-23 15:17:06 -0600

    the method gameSetUp is now created

--- data/gamem.py
+++ data/gamem.py
@@ -4,9 +4,11 @@ here we first will generate the virus to make a sick
 we give it back."""
 import dataman
 import errors
-
+import random
 
 class GameM:
+
+    sickNumber: int
 
     def __init__(self):
 
@@ -15,6 +17,15 @@ class GameM:
 
         for creature in self.data.availableCreatures:
             self.allPoly.append(self.data.getPolyObj(creature))
+
+    def gameSetUp(self, sickNumber):
+        """sickNumber is the number of sicks for the game"""
+        self.sickNumber = sickNumber
+
+    def generateVirus(self):
+
+
+
 
 
 
