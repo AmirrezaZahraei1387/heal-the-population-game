41e98da2f9207b866acd86eff69f6d874fb79e6b
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-23 14:46:46 -0600

    errors is added

--- data/gamem.py
+++ data/gamem.py
@@ -3,7 +3,16 @@ game that will do all the backend calculations.
 here we first will generate the virus to make a sick
 we give it back."""
 import dataman
+import polys_ever
 
 
 data = dataman.DataM()
 
+
+class GameM:
+
+    def __init__(self):
+        try:
+            data = dataman.DataM()
+        except Exception:
+            raise
---
+++ errors.py
@@ -0,0 +1,6 @@
+
+
+class CanNotOpenDataError(Exception):
+
+    def __init__(self, msg):
+        super().__init__(msg)
